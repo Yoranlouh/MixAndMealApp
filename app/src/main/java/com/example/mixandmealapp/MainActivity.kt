112e52caa34e2c5f93fde5d9b84c26497d7101ff
Yoran <yoranlouh@hotmail.com>
2025-11-16 15:46:45 +0100

    Structure

---
+++ app/src/main/java/com/example/mixandmealapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mixandmealapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.mixandmealapp.ui.navigation.AppNavigation
+import com.example.mixandmealapp.ui.theme.MixAndMealAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        setContent {
+            MixAndMealAppTheme {
+                AppNavigation()
+            }
+        }
+    }
+}
