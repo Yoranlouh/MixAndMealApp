93c3a25e4493ec5b3a38d9a759f4db3520da3304
BartKuijs <bartkuijs@live.nl>
2026-01-11 11:21:34 +0100

    feat: added data classes for recipe and ingredient requests

---
+++ app/src/main/java/com/example/mixandmealapp/models/requests/IngredientIDRequest.kt
@@ -0,0 +1,6 @@
+package com.example.mixandmealapp.models.requests
+
+@kotlinx.serialization.Serializable
+data class IngredientIDRequest(
+    val ingredientName : String
+)
---
+++ app/src/main/java/com/example/mixandmealapp/models/requests/RecipeIDRequest.kt
@@ -0,0 +1,6 @@
+package com.example.mixandmealapp.models.requests
+
+@kotlinx.serialization.Serializable
+data class RecipeIDRequest(
+    val recipeId: Int
+)
